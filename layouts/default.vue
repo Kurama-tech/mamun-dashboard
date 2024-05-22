dca875abbbb6bd77b7904325f3c0db0b0ed3152e
Meer Sawood <msawood@redhat.com>
2024-05-22 11:22:08 +0530

    change name

--- layouts/default.vue
+++ layouts/default.vue
@@ -135,7 +135,7 @@ export default {
       miniVariant: true,
       right: true,
       rightDrawer: false,
-      title: 'DumDum Dashboard'
+      title: 'AppTech Marketing'
     }
   },
   methods: {
